bf9bbc3d313b82d3c31f3d926faa9c51a8543dc1
matan amzaleg <matanamz@gmail.com>
2022-02-12 22:06:13 +0200

    finallll course repositry

--- app.js
+++ app.js
@@ -185,8 +185,11 @@ req.login(user, function(err){
 })
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000,function(){
-  console.log("server is running on port 3000");
+app.listen(port,function(){
+  console.log("server is running on port");
 })
